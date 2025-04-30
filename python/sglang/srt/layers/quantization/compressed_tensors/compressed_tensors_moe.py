cf4b07ef4f5c551bbaf86f9d713cecff102ea1eb
nytopop <ericizoita@gmail.com>
2025-04-29 20:27:16 -0700

    fix imports for WNA16

--- python/sglang/srt/layers/quantization/compressed_tensors/compressed_tensors_moe.py
+++ python/sglang/srt/layers/quantization/compressed_tensors/compressed_tensors_moe.py
@@ -29,6 +29,16 @@ if not _is_cuda:
 
 try:
     import vllm
+    from vllm.model_executor.layers.quantization.compressed_tensors.schemes import (
+        W4A16SPARSE24_SUPPORTED_BITS,
+        WNA16_SUPPORTED_BITS,
+        CompressedTensors24,
+        CompressedTensorsW4A16Sparse24,
+        CompressedTensorsW8A8Int8,
+        CompressedTensorsW8A16Fp8,
+        CompressedTensorsWNA16,
+    )
+    from vllm import _custom_ops as vllm_ops
 
     VLLM_AVAILABLE = True
 except ImportError:
